67f8ab4ab4d45d01321c6b619a5859bc1e4052b2
Dmitry Matora <dmitry.matora@gmail.com>
2022-12-06 11:05:04 +0600

    Implemented alpha version of API integration for quickflash

--- src/baseCommand.ts
+++ src/baseCommand.ts
@@ -5,9 +5,23 @@ import path = require('path')
 export type Flags<T extends typeof Command> = Interfaces.InferredFlags<typeof BaseCommand['globalFlags'] & T['flags']>
 
 export abstract class BaseCommand<T extends typeof Command> extends Command {
+  protected apiUrl = 'https://ra-protocol-api.vercel.app/api'
+
   protected globalFlags: any
 
   protected flags!: Flags<T>
+
+  protected processApiError = (error: any) => {
+    if (error.response) {
+      if (error.response.status >= 400 && error.response.status < 500) {
+        this.warn(error.response.data)
+      } else {
+        this.error(error.response.data)
+      }
+    } else {
+      this.error('Failed communicating with the server. Check your internet connection')
+    }
+  }
 
   public async init(): Promise<void> {
     const configFilename = path.join(this.config.configDir, 'config.json')
